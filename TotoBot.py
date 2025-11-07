dfd2fb854171358ae7de2f4e8c57023baed73df4
ZinWin <zinwin.1994@gmail.com>
2025-11-07 19:30:46 +0800

    Update TotoBot.py

--- TotoBot.py
+++ TotoBot.py
@@ -272,7 +272,7 @@ async def main():
 
     scheduler.start()
     webhook_url = os.getenv("WEBHOOK_URL")
-    port = int(os.getenv("port", 8000))
+    port = int(os.getenv("PORT", 8000))
     await app.start_webhook(
         listen="0.0.0.0",
         port=port,
